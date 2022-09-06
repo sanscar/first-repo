8caadf1aa996ffcaef6cfd4c06de6fb0b98394fa
don <email123>
2022-09-06 18:32:42 +1400

    initial commit

---
+++ sum.js
@@ -0,0 +1,9 @@
+// store input numbers
+const num1 = parseInt(prompt('12'));
+const num2 = parseInt(prompt('12'));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
