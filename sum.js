a6f3dd2e4da01ae632f4d562814d5d3e3e322c74
don <email123>
2022-09-06 18:47:32 +1400

    initial commit

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
 // store input numbers
 const num1 = parseInt(prompt('12'));
-const num2 = parseInt(prompt('12'));
+const num2 = parseInt(prompt('25'));
 
 //add two numbers
 const sum = num1 + num2;
